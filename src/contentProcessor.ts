35dba9c33f1f673fe388fabe75262b74b0dc73d0
Packy <lpreterite@126.com>
2022-11-21 22:17:41 +0800

    Only use the MD5 of the image data as the name

--- src/contentProcessor.ts
+++ src/contentProcessor.ts
@@ -78,45 +78,17 @@ async function chooseFileName(
   if (!fileExt) {
     return { fileName: "", needWrite: false };
   }
-  // if there is no anchor try get file name from url
-  if (!baseName) {
-    baseName = md5(contentData);
-  }
-  // if there is no part for file name from url use name template
-  if (!baseName) {
-    baseName = FILENAME_TEMPLATE;
-  }
 
-  // if filename already ends with correct extension, remove it to work with base name
-  if (baseName.endsWith(`.${fileExt}`)) {
-    baseName = baseName.slice(0, -1 * (fileExt.length + 1));
-  }
-
-  baseName = cleanFileName(baseName);
-
-  let fileName = "";
+  let fileName = pathJoin(dir, `${md5(contentData)}.${fileExt}`);
   let needWrite = true;
-  let index = 0;
-  while (!fileName && index < MAX_FILENAME_INDEX) {
-    const suggestedName = index
-      ? pathJoin(dir, `${baseName}-${index}.${fileExt}`)
-      : pathJoin(dir, `${baseName}.${fileExt}`);
-
-    if (await adapter.exists(suggestedName, false)) {
-      linkHashes.ensureHashGenerated(link, contentData);
-
-      const fileData = await adapter.readBinary(suggestedName);
-
-      if (linkHashes.isSame(link, fileData)) {
-        fileName = suggestedName;
-        needWrite = false;
-      }
-    } else {
-      fileName = suggestedName;
+  if (await adapter.exists(fileName, false)) {
+    linkHashes.ensureHashGenerated(link, contentData);
+    const fileData = await adapter.readBinary(fileName);
+    if (linkHashes.isSame(link, fileData)) {
+      needWrite = false;
     }
-
-    index++;
   }
+
   if (!fileName) {
     throw new Error("Failed to generate file name for media file.");
   }
